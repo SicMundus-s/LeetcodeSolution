3ce6ab625369ce4ddfa90a1fba7e0f5fed1afb50
mrNRaven-hub <nikita.volkov0505@mail.ru>
2023-02-15 22:11:38 +0300

    PascalsTriangle

--- src/ArraysAndHashing/Middle/GroupAnagrams.java
+++ src/ArraysAndHashing/Middle/GroupAnagrams.java
@@ -13,22 +13,20 @@ public class GroupAnagrams {
         System.out.println(groupAnagrams(strs));
     }
 
-    public static List<List<String>> groupAnagrams(String[] str) {
-        int n=str.length;
+    public static List<List<String>> groupAnagrams(String[] strs) {
         HashMap<String,ArrayList<String>> hm=new HashMap<>();
         List<List<String>> list=new ArrayList<>();
 
-        for(int i=0;i<n;i++){
-            char [] ch=str[i].toCharArray();
+        for (String s : strs) {
+            char[] ch = s.toCharArray();
             Arrays.sort(ch);
-            String temp=new String(ch);
-            if(hm.containsKey(temp)){
-                hm.get(temp).add(str[i]);
-            }
-            else{
-                ArrayList<String> t=new ArrayList<>();
-                t.add(str[i]);
-                hm.put(temp,t);
+            String temp = new String(ch);
+            if (hm.containsKey(temp)) {
+                hm.get(temp).add(s);
+            } else {
+                ArrayList<String> t = new ArrayList<>();
+                t.add(s);
+                hm.put(temp, t);
             }
         }
 
